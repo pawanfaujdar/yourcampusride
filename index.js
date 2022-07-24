c1d778eb371889365e72a55af1eb020b004cfa3f
Pawan <Pawankumar.vandemataram@gmail.com>
2022-07-24 22:49:49 +0530

    campus ride

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const path = require('path');
+const app =express();
+
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+// app.use(express.static(path.join(__dirname, '/assets'))) 
+app.use( express.static( "assets" ));
+
+
+app.get('/', function(req,res){
+    // res.send('its running');
+    res.render('home');
+});
+
+app.listen(port,function(err){
+    if(err){
+        console.log('error in running');
+    }
+    else{
+        console.log('running server at port :',port);
+    }
+})
---
+++ routes/msg.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+
+const Msg = require("../models/message");
+
